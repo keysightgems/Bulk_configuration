8de2d03a00d41a181223ccd855dfb6bc143d2a6e
Mohan Reddy <40664949+keysightgems@users.noreply.github.com>
2021-05-06 12:06:54 +0530

    Update bulk_config_test.py

--- bulk_config_test.py
+++ bulk_config_test.py
@@ -1,15 +1,9 @@
 from create_excel import API_Sheet
 from bulk_config import BulkConfig
-from datetime import datetime
 
-# excel=API_Sheet()
-# excelName=excel.create_excel(DHCP={[10]*100})
+excel=API_Sheet()
+excelName=excel.create_excel()
 
 tgnObj = BulkConfig('127.0.0.1', clearConfig=True)
-startTime = datetime.now()
-# tgnObj.bulk_config(excelName)
-tgnObj.bulk_config('Generate_IxNetwork_Config-original-mohan.xlsx')
-tgnObj.create_trafficitems('Generate_IxNetwork_Config-original-mohan.xlsx')
-endTime = datetime.now()
-print("Start Time:", startTime)
-print("End Time:", endTime.now())
+tgnObj.bulk_config(excelName)
+tgnObj.create_trafficitems(excelName)
