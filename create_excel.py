381028a7bb6e7f6337010c8b505057d4aaea6094
mohan <mohan.reddy@keysight.com>
2021-05-12 20:06:05 +0530

    Adding DHCP

--- create_excel.py
+++ create_excel.py
@@ -3,7 +3,7 @@ import pandas as pd
 class API_Sheet():
     def __init__(self, build_information=None, base=None, physical=None, devicegroup=None, ipv4_ethernet=None,
                  ipv6_ethernet=None, ipv4_bgp=None, ipv6_bgp=None, ipv4_loopback=None, bgp_capabilities=None, ipv4_ospf=None,
-                 ipv6_ospf=None, isis=None, networkgroup=None, igmp_host=None, igmp_querier=None, traffic=None, packet_editor=None, **kwargs):
+                 ipv6_ospf=None, isis=None, dhcp_ipv4=None, dhcp_ipv6=None, dhcp_serverv4=None, dhcp_serverv6=None, networkgroup=None, igmp_host=None, igmp_querier=None, traffic=None, packet_editor=None, **kwargs):
         self.Build_information = build_information
         self.Base = base
         self.Physical = physical
@@ -17,33 +17,24 @@ class API_Sheet():
         self.IPv4_OSPF = ipv4_ospf
         self.IPv6_OSPF = ipv6_ospf
         self.ISIS = isis
+        self.DHCP_Ipv4 = dhcp_ipv4
+        self.DHCP_Ipv6 = dhcp_ipv6
+        self.DHCP_Serverv4 = dhcp_serverv4
+        self.DHCP_Serverv6 = dhcp_serverv6
         self.Network_Group = networkgroup
         self.IGMP_Host = igmp_host
         self.IGMP_Querier = igmp_querier
         self.Traffic = traffic
         self.packet_editor = packet_editor
-        # create Build Information
-        # if self.Build_information == None or self.Build_information == 'default':
-        #     buildDataDict = {}
-        #     excelParameters = [base, physical, devicegroup, ipv4_ethernet, ipv6_ethernet, ipv4_bgp, ipv6_bgp, ipv4_loopback,
-        #                        bgp_capabilities, ipv4_ospf, ipv6_ospf, isis, networkgroup, igmp_host, igmp_querier, traffic, packet_editor]
-        #     [buildDataDict.update({excelparam.capitalize(): excelparam}) for excelparam in excelParameters if excelparam != None]
-        #     self.buildData = {'Include': [], 'Description':[],  'Worksheet': [], 'Status': []}
-        #     # import pdb;pdb.set_trace()
-        #     for buildData, value in buildDataDict.items():
-        #         self.buildData['Include'].append('yes')
-        #         self.buildData['Description'].append(buildData)
-        #         self.buildData['Worksheet'].append(buildData)
-        #         self.buildData['Status'].append('Completed')
 
     def create_excel(self,excelname='Generate_IxNetwork_Config.xlsx'):
         if self.Build_information == 'default' or self.Build_information == None:
-            build_data = pd.DataFrame({'Include': ['yes']*17, 'Description':['Base Variables','Assigning Physical Ports and Topology','Assigning Device Groups to Topologies',\
+            build_data = pd.DataFrame({'Include': ['yes']*21, 'Description':['Base Variables','Assigning Physical Ports and Topology','Assigning Device Groups to Topologies',\
                                                                              'Configuring IPv4 and Ethernet Information','Configuring IPv6 and Ethernet Information','IPv4 BGP Configuration','IPv6 BGP Configuration',\
                                                                              'IPv4 BGP on Loopback Configuation','BGP Capabilities','IPv4 OSPF Configuration','IPv6 OSPF Configuration','ISIS Configuration','Network Group Configuration',\
-                                                                             'IGMP Senders group Configuration','IGMP Receivers Group Configuration','Traffic Flow setup','Packet editor'],\
+                                                                             'IGMP Senders group Configuration','IGMP Receivers Group Configuration','Traffic Flow setup','Packet editor', 'DHCP_Ipv4', 'DHCP_Ipv6', 'DHCP_Serverv4', 'DHCP_Serverv6'],\
                                        'Worksheet':['Base','Physical','Devicegroup','IPv4_Ethernet','IPv6_Ethernet','IPv4_BGP','IPv6_BGP','IPv4_Loopback_BGP','BGP_Capabilities',\
-                                                    'IPv4_OSPF','IPv6_OSPF','ISIS','Network_Group','IGMP_Host','IGMP_Querier','Traffic','packet_editor'], 'Status':['Completed']*17})
+                                                    'IPv4_OSPF','IPv6_OSPF','ISIS','Network_Group','IGMP_Host','IGMP_Querier','Traffic','packet_editor','DHCP_Ipv4','DHCP_Ipv6','DHCP_Serverv4','DHCP_Serverv6'], 'Status':['Completed']*21})
             # build_data = pd.DataFrame(self.buildData)
         else:
             if self.Build_information != None:
@@ -112,6 +103,28 @@ class API_Sheet():
         else:
             if self.ISIS != None:
                 isis_data = pd.DataFrame(self.ISIS)
+
+        if self.DHCP_Ipv4 == 'default':
+            dhcp_data = pd.DataFrame({'Device Group': ['Device Group 1']})
+        else:
+            if self.DHCP_Ipv4 != None:
+                dhcp_data = pd.DataFrame(self.DHCP_Ipv4)
+        if self.DHCP_Serverv4 == 'default':
+            dhcpserverv4_data = pd.DataFrame({'Device Group': ['Device Group 2']})
+        else:
+            if self.DHCP_Serverv4 != None:
+                dhcpserverv4_data = pd.DataFrame(self.DHCP_Serverv4)
+        if self.DHCP_Ipv6 == 'default':
+            dhcpv6_data = pd.DataFrame({'Device Group': ['Device Group 1', 'Device Group 2']})
+        else:
+            if self.DHCP_Ipv6 != None:
+                dhcpv6_data = pd.DataFrame(self.DHCP_Ipv6)
+        if self.DHCP_Serverv6 == 'default':
+            dhcpserverv6_data = pd.DataFrame({'Device Group': ['Device Group 2']})
+        else:
+            if self.DHCP_Serverv6 != None:
+                dhcpserverv6_data = pd.DataFrame(self.DHCP_Serverv6)
+
         if self.Network_Group == 'default':
             network_data = pd.DataFrame({'Device Group': ['Device Group 1', 'Device Group 2'], 'Name': ['Network Group 1', 'Network Group 2'], 'IP Version': ['ipv4', 'ipv4'], 'Multiplier':[1, 1], 'Protocol':['ospfv2', 'ospfv2']})
         else:
@@ -165,6 +178,14 @@ class API_Sheet():
             ospfv3_data.to_excel(writer, sheet_name='IPv6_OSPF', index=False)
         if self.ISIS != None:
             isis_data.to_excel(writer, sheet_name='ISIS', index=False)
+        if self.DHCP_Ipv4 != None:
+            dhcp_data.to_excel(writer, sheet_name='DHCP_Ipv4', index=False)
+        if self.DHCP_Serverv4 !=None:
+            dhcpserverv4_data.to_excel(writer, sheet_name='DHCP_Serverv4', index=False)
+        if self.DHCP_Ipv6 != None:
+            dhcp_data.to_excel(writer, sheet_name='DHCP_Ipv6', index=False)
+        if self.DHCP_Serverv6 !=None:
+            dhcpserverv6_data.to_excel(writer, sheet_name='DHCP_Serverv6', index=False)
         if self.Network_Group != None:
             network_data.to_excel(writer, sheet_name='Network_Group', index=False)
         if self.IGMP_Host != None:
