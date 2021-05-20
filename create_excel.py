1d9249ead924bc3e8e36b4250eba6b9957529f5c
mohan <mohan.reddy@keysight.com>
2021-05-20 15:45:38 +0530

    Added MLD code and modified excel sheet

--- create_excel.py
+++ create_excel.py
@@ -3,7 +3,7 @@ import pandas as pd
 class API_Sheet():
     def __init__(self, build_information=None, base=None, physical=None, devicegroup=None, ipv4_ethernet=None,
                  ipv6_ethernet=None, ipv4_bgp=None, ipv6_bgp=None, ipv4_loopback=None, bgp_capabilities=None, ipv4_ospf=None,
-                 ipv6_ospf=None, isis=None, dhcp_ipv4=None, dhcp_ipv6=None, dhcp_serverv4=None, dhcp_serverv6=None, networkgroup=None, igmp_host=None, igmp_querier=None, traffic=None, packet_editor=None, **kwargs):
+                 ipv6_ospf=None, isis=None, dhcp_ipv4=None, ldp=None, dhcp_ipv6=None, dhcp_serverv4=None, dhcp_serverv6=None, networkgroup=None, igmp_host=None, igmp_querier=None, mld_host=None, mld_querier=None, traffic=None, packet_editor=None, **kwargs):
         self.Build_information = build_information
         self.Base = base
         self.Physical = physical
@@ -19,22 +19,25 @@ class API_Sheet():
         self.ISIS = isis
         self.DHCP_Ipv4 = dhcp_ipv4
         self.DHCP_Ipv6 = dhcp_ipv6
+        self.LDP = ldp
         self.DHCP_Serverv4 = dhcp_serverv4
         self.DHCP_Serverv6 = dhcp_serverv6
         self.Network_Group = networkgroup
         self.IGMP_Host = igmp_host
         self.IGMP_Querier = igmp_querier
+        self.MLD_Querier = mld_querier
+        self.MLD_Host = mld_host
         self.Traffic = traffic
         self.packet_editor = packet_editor
 
     def create_excel(self,excelname='Generate_IxNetwork_Config.xlsx'):
         if self.Build_information == 'default' or self.Build_information == None:
-            build_data = pd.DataFrame({'Include': ['yes']*21, 'Description':['Base Variables','Assigning Physical Ports and Topology','Assigning Device Groups to Topologies',\
+            build_data = pd.DataFrame({'Include': ['yes']*24, 'Description':['Base Variables','Assigning Physical Ports and Topology','Assigning Device Groups to Topologies',\
                                                                              'Configuring IPv4 and Ethernet Information','Configuring IPv6 and Ethernet Information','IPv4 BGP Configuration','IPv6 BGP Configuration',\
                                                                              'IPv4 BGP on Loopback Configuation','BGP Capabilities','IPv4 OSPF Configuration','IPv6 OSPF Configuration','ISIS Configuration','Network Group Configuration',\
-                                                                             'IGMP Senders group Configuration','IGMP Receivers Group Configuration','Traffic Flow setup','Packet editor', 'DHCP_Ipv4', 'DHCP_Ipv6', 'DHCP_Serverv4', 'DHCP_Serverv6'],\
+                                                                             'IGMP Senders group Configuration','IGMP Receivers Group Configuration','Traffic Flow setup','Packet editor', 'DHCP_Ipv4', 'DHCP_Ipv6', 'DHCP_Serverv4', 'DHCP_Serverv6', 'LDP', 'MLD_Host', 'MLD_Querier'],\
                                        'Worksheet':['Base','Physical','Devicegroup','IPv4_Ethernet','IPv6_Ethernet','IPv4_BGP','IPv6_BGP','IPv4_Loopback_BGP','BGP_Capabilities',\
-                                                    'IPv4_OSPF','IPv6_OSPF','ISIS','Network_Group','IGMP_Host','IGMP_Querier','Traffic','packet_editor','DHCP_Ipv4','DHCP_Ipv6','DHCP_Serverv4','DHCP_Serverv6'], 'Status':['Completed']*21})
+                                                    'IPv4_OSPF','IPv6_OSPF','ISIS','Network_Group','IGMP_Host','IGMP_Querier','Traffic','packet_editor','DHCP_Ipv4','DHCP_Ipv6','DHCP_Serverv4','DHCP_Serverv6', 'LDP', 'MLD_Host', 'MLD_Querier'], 'Status':['Completed']*24})
             # build_data = pd.DataFrame(self.buildData)
         else:
             if self.Build_information != None:
@@ -124,22 +127,38 @@ class API_Sheet():
         else:
             if self.DHCP_Serverv6 != None:
                 dhcpserverv6_data = pd.DataFrame(self.DHCP_Serverv6)
-
+        if self.LDP == 'default':
+            ldp_data = pd.DataFrame({'Device Group': ['Device Group 1', 'Device Group 2'], 'IP Version':['ipv4','ipv6']})
+        else:
+            if self.LDP != None:
+                ldp_data = pd.DataFrame(self.LDP)
         if self.Network_Group == 'default':
             network_data = pd.DataFrame({'Device Group': ['Device Group 1', 'Device Group 2'], 'Name': ['Network Group 1', 'Network Group 2'], 'IP Version': ['ipv4', 'ipv4'], 'Multiplier':[1, 1], 'Protocol':['ospfv2', 'ospfv2']})
         else:
             if self.Network_Group != None:
                 network_data = pd.DataFrame(self.Network_Group)
         if self.IGMP_Host == 'default':
-            host_data = pd.DataFrame({'Device Group': ['Device Group 1', 'Device Group 2']})
+            host_data = pd.DataFrame({'Device Group': ['Device Group 1']})
         else:
             if self.IGMP_Host != None:
                 host_data = pd.DataFrame(self.IGMP_Host)
+        if self.MLD_Host == 'default':
+            mldhost_data = pd.DataFrame({'Device Group': ['Device Group 1']})
+        else:
+            if self.MLD_Host != None:
+                mldhost_data = pd.DataFrame(self.MLD_Host)
+
         if self.IGMP_Querier == 'default':
-            querier_data = pd.DataFrame({'Device Group': ['Device Group 1', 'Device Group 2']})
+            querier_data = pd.DataFrame({'Device Group': ['Device Group 2']})
         else:
             if self.IGMP_Querier != None:
                 querier_data = pd.DataFrame(self.IGMP_Querier)
+
+        if self.MLD_Querier == 'default':
+            mldquerier_data = pd.DataFrame({'Device Group': ['Device Group 2']})
+        else:
+            if self.MLD_Querier != None:
+                mldquerier_data = pd.DataFrame(self.MLD_Querier)
         if self.Traffic == 'default':
             traffic_data = pd.DataFrame({'Traffic name': ['Device Group 1'], 'Type':['ipv4'], 'bi-directional': ['yes'], 'Source': ['Device Group 1'], 'Destination': ['Device Group 2']})
         else:
@@ -183,15 +202,21 @@ class API_Sheet():
         if self.DHCP_Serverv4 !=None:
             dhcpserverv4_data.to_excel(writer, sheet_name='DHCP_Serverv4', index=False)
         if self.DHCP_Ipv6 != None:
-            dhcp_data.to_excel(writer, sheet_name='DHCP_Ipv6', index=False)
+            dhcpv6_data.to_excel(writer, sheet_name='DHCP_Ipv6', index=False)
         if self.DHCP_Serverv6 !=None:
             dhcpserverv6_data.to_excel(writer, sheet_name='DHCP_Serverv6', index=False)
+        if self.LDP !=None:
+            ldp_data.to_excel(writer, sheet_name='LDP', index=False)
         if self.Network_Group != None:
             network_data.to_excel(writer, sheet_name='Network_Group', index=False)
         if self.IGMP_Host != None:
             host_data.to_excel(writer, sheet_name='IGMP_Host', index=False)
+        if self.MLD_Host != None:
+            mldhost_data.to_excel(writer, sheet_name='MLD_Host', index=False)
         if self.IGMP_Querier != None:
             querier_data.to_excel(writer, sheet_name='IGMP_Querier', index=False)
+        if self.MLD_Querier != None:
+            mldquerier_data.to_excel(writer, sheet_name='MLD_Querier', index=False)
         if self.Traffic != None:
             traffic_data.to_excel(writer, sheet_name='Traffic', index=False)
         if self.packet_editor != None:
